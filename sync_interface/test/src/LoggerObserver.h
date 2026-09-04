bc819f91725ac68c2514e785dba608336ca3ac2f
Celia Qiang <celia.qiang@qualitrolcorp.com>
2026-09-04 12:25:27 +0100

    Udate test/scr ,get the correct original h files

--- sync_interface/test/src/LoggerObserver.h
+++ sync_interface/test/src/LoggerObserver.h
@@ -8,10 +8,6 @@ class LoggerObserver : public IObserver
 {
 public:
 
-    void OnConnected();
-    void OnData(int id);
-    void OnData(int id, double timestamp);
-    void OnError(int err_code) override;
-    void OnData(int id, double timestamp) override;
-    void OnError(int err_code) override;
+    void OnConnected() override;
+    void OnData(int id) override;
 };
--- sync_interface/test/src/strategy/StrategyObserver.h
+++ sync_interface/test/src/strategy/StrategyObserver.h
@@ -6,11 +6,7 @@ class StrategyObserver : public IObserver
 {
 public:
 
-    void OnConnected();
-    void OnData(int id);
-    void OnData(int id, double timestamp);
-    void OnError(int err_code) override;
-    void OnData(int id, double timestamp) override;
-    void OnError(int err_code) override;
+    void OnConnected() override;
+    void OnData(int id) override;
 };
 ``
